64f697842cb0929626875777aa7a9b3e98fc842f
BootsStribling <williamstriblingenterprises@gmail.com>
2022-05-10 15:12:35 -0500

    add some more listings && fix landing page image distortions by deleting images on certain heights

--- routes/realtors.js
+++ routes/realtors.js
@@ -1,29 +1,27 @@
 import { Router } from 'express'
 import { isLoggedIn } from '../middleware/middleware.js'
 import * as realtorsCtrl from '../controllers/realtors.js'
-import * as indexCtrl from '../controllers/index.js'
 
 const router = Router()
 
 //*GETS*//
-router.get('/', indexCtrl.index)
-// //GET - localhost:3000/realtors
-// router.get('/', isLoggedIn, realtorsCtrl.index)
-// //GET - localhost:3000/realtors/new
-// router.get('/new', isLoggedIn, realtorsCtrl.new)
-// //GET - localhost:3000/realtors/:id
-// router.get('/:id', isLoggedIn, realtorsCtrl.show)
-// //GET - localhost:3000/realtors/:id/edit
-// router.get('/:id/edit', isLoggedIn, realtorsCtrl.edit)
+//GET - localhost:3000/realtors
+router.get('/', isLoggedIn, realtorsCtrl.index)
+//GET - localhost:3000/realtors/new
+router.get('/new', isLoggedIn, realtorsCtrl.new)
+//GET - localhost:3000/realtors/:id
+router.get('/:id', isLoggedIn, realtorsCtrl.show)
+//GET - localhost:3000/realtors/:id/edit
+router.get('/:id/edit', isLoggedIn, realtorsCtrl.edit)
 
-// //POST - localhost:3000/realtors
-// router.post('/', isLoggedIn, realtorsCtrl.create)
+//POST - localhost:3000/realtors
+router.post('/', isLoggedIn, realtorsCtrl.create)
 
-// //PUT - localhost:3000/realtors/:id
-// router.put('/:id', isLoggedIn, realtorsCtrl.update)
+//PUT - localhost:3000/realtors/:id
+router.put('/:id', isLoggedIn, realtorsCtrl.update)
 
-// //DELETE - localhost:3000/realtors/:id
-// router.delete('/:id', isLoggedIn, realtorsCtrl.delete)
+//DELETE - localhost:3000/realtors/:id
+router.delete('/:id', isLoggedIn, realtorsCtrl.delete)
 
 
 export {
